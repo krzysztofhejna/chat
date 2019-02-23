f3837f1b4748ebe817e235ab757b6cf798ff2f3e
krzysztofhejna <khejna15@wp.pl>
2019-02-23 18:42:01 +0100

    Change app styles

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env) => {
+  return {
+    mode: env || 'production',
+    entry: './client/index.js',
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'app.bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          options: {
+            plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            { loader: 'style-loader'},
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+      template: 'client/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })],
+    devServer: {
+      proxy: {
+        '/socket.io': {
+          target: 'http://localhost:3000',
+          ws: true
+        }
+      }
+    }
+  }
+};
